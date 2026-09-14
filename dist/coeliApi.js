e577eb125cc6dfb206a026f521c4b0cb7b2aa0a2
Néstor PS <nestorps.developer@gmail.com>
2026-09-14 12:17:48 +0200

    Migrate client to api.coeli.cat
    - Point requests to https://api.coeli.cat
    - Remove the trailing slash before the query string when fetching a
      controlled search, which api.coeli.cat rejects with "Invalid endpoint"
    - Stop formatting entities: api.coeli.cat already returns plain values
      (no values wrappers nor label.value), so responses are passed to
      mapFunction as they are
    - Return undefined from getControlledSearch when the request fails,
      like the other methods
    - Rebuild dist/coeliApi

--- dist/coeliApi.js
+++ dist/coeliApi.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -11,12 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CoeliApi = void 0;
 const isomorphic_fetch_1 = __importDefault(require("isomorphic-fetch"));
-const formatUtils_1 = require("./formatUtils");
 class CoeliApi {
     constructor(tenant, token) {
-        this.coeliFetch = (partialUrl, language, method = 'GET', mapFunction, body) => __awaiter(this, void 0, void 0, function* () {
-            const url = `https://app.coeli.cat/coeli/${this.tenant}${partialUrl}`;
+        this.coeliFetch = (partialUrl_1, language_1, ...args_1) => __awaiter(this, [partialUrl_1, language_1, ...args_1], void 0, function* (partialUrl, language, method = 'GET', mapFunction, body) {
+            const url = `https://api.coeli.cat/coeli/${this.tenant}${partialUrl}`;
             const headers = {
                 'Accept-Language': language,
                 Authorization: `Authorization ${this.token}`,
@@ -29,7 +30,7 @@ class CoeliApi {
                 method,
             };
             try {
-                const response = yield isomorphic_fetch_1.default(url, requestInit);
+                const response = yield (0, isomorphic_fetch_1.default)(url, requestInit);
                 if (response.status >= 400) {
                     throw new Error(`An error occurred trying to request Coeli api, url:\n ${url}\n with status ${response.status}, ${response.statusText}: \n${JSON.stringify(requestInit)}`);
                 }
@@ -45,18 +46,20 @@ class CoeliApi {
         });
         this.getControlledSearch = (language, controlledSearchResponse, mapFunction, facets, page, facetModes) => __awaiter(this, void 0, void 0, function* () {
             const partialUrl = `/${controlledSearchResponse.entityTypeName}/search/${controlledSearchResponse.id}${facets
-                ? `/?${page
+                ? `?${page
                     ? `limit=${page.limit}&offset=${page.offset}`
                     : 'limit=25&offset=0'}` +
                     '&facet=' +
                     facets.join(',')
-                : `/?${page
+                : `?${page
                     ? `limit=${page.limit}&offset=${page.offset}`
                     : 'limit=25&offset=0'}`}${facetModes ? '&facetMode=' + facetModes.join(',') : ''}`;
             const getSearchResponse = yield this.coeliFetch(partialUrl, language, 'GET', (x) => x);
-            return mapFunction(Object.assign({}, getSearchResponse, {
-                entities: getSearchResponse.entities.map((e) => formatUtils_1.formattedEntity(language, e)),
-            }, { url: '/' + controlledSearchResponse.self.href.split('/').slice(3).join('/') }));
+            // coeliFetch resolves undefined on request errors
+            if (!getSearchResponse)
+                return undefined;
+            // api.coeli.cat already returns plain values, no formatting needed
+            return mapFunction(Object.assign(Object.assign({}, getSearchResponse), { url: '/' + controlledSearchResponse.self.href.split('/').slice(3).join('/') }));
         });
         this.createAndGetControlledSearch = (language, entity, search, mapFunction, facets, page, facetModes) => __awaiter(this, void 0, void 0, function* () {
             const controlledSearchResponse = yield this.createControlledSearch(language, entity, search);
@@ -78,24 +81,22 @@ class CoeliApi {
                     return acc;
                 }
                 else {
-                    const arr = yield this.getControlledSearch(language, controlledSearchResponse, mapFunction, facets, Object.assign({}, page, { offset: page.offset + page.limit }));
-                    return yield loop(acc.concat(arr.entities), Object.assign({}, arr.page, { offset: arr.page.offset }));
+                    const arr = yield this.getControlledSearch(language, controlledSearchResponse, mapFunction, facets, Object.assign(Object.assign({}, page), { offset: page.offset + page.limit }));
+                    return yield loop(acc.concat(arr.entities), Object.assign(Object.assign({}, arr.page), { offset: arr.page.offset }));
                 }
             });
             const coeliEntities = yield loop(firstBatch.entities, firstBatch.page);
-            return Object.assign({}, firstBatch, { entities: coeliEntities });
+            return Object.assign(Object.assign({}, firstBatch), { entities: coeliEntities });
         });
         this.getEntityBySlug = (language, entity, slug, mapFunction) => __awaiter(this, void 0, void 0, function* () {
-            return yield this.coeliFetch(`/${entity}/slugs/${slug}`, language, 'GET', (e) => mapFunction(formatUtils_1.formattedEntity(language, e)));
+            return yield this.coeliFetch(`/${entity}/slugs/${slug}`, language, 'GET', (e) => mapFunction(e));
         });
         this.getEntityById = (language, entity, id, mapFunction) => __awaiter(this, void 0, void 0, function* () {
-            return yield this.coeliFetch(`/${entity}/${id}`, language, 'GET', (e) => mapFunction(formatUtils_1.formattedEntity(language, e)));
+            return yield this.coeliFetch(`/${entity}/${id}`, language, 'GET', (e) => mapFunction(e));
         });
         this.getEntities = (language, entity, mapFunction) => __awaiter(this, void 0, void 0, function* () {
             const coeliEntityGetResponse = yield this.coeliFetch(`/${entity}/`, language, 'GET', (x) => {
-                return Object.assign({}, x, { entities: x.entities
-                        .map((e) => formatUtils_1.formattedEntity(language, e))
-                        .map(mapFunction) });
+                return Object.assign(Object.assign({}, x), { entities: x.entities.map((e) => mapFunction(e)) });
             });
             return coeliEntityGetResponse;
         });
--- src/coeliApi.ts
+++ src/coeliApi.ts
@@ -7,7 +7,7 @@ import {
   Page,
   FacetMode,
 } from './model';
-import { Entity, formattedEntity } from './formatUtils';
+import { Entity } from './formatUtils';
 
 export type AcceptedLanguage = 'es' | 'ca' | 'en' | 'fr';
 
@@ -26,7 +26,7 @@ export class CoeliApi {
     mapFunction: (r: R) => T,
     body?: object
   ): Promise<T> => {
-    const url = `https://app.coeli.cat/coeli/${this.tenant}${partialUrl}`;
+    const url = `https://api.coeli.cat/coeli/${this.tenant}${partialUrl}`;
 
     const headers: HeadersInit = {
       'Accept-Language': language,
@@ -84,14 +84,14 @@ export class CoeliApi {
       controlledSearchResponse.id
     }${
       facets
-        ? `/?${
+        ? `?${
             page
               ? `limit=${page.limit}&offset=${page.offset}`
               : 'limit=25&offset=0'
           }` +
           '&facet=' +
           facets.join(',')
-        : `/?${
+        : `?${
             page
               ? `limit=${page.limit}&offset=${page.offset}`
               : 'limit=25&offset=0'
@@ -101,13 +101,11 @@ export class CoeliApi {
       GetSearchResponse<Entity>,
       GetSearchResponse<Entity>
     >(partialUrl, language, 'GET', (x) => x);
+    // coeliFetch resolves undefined on request errors
+    if (!getSearchResponse) return undefined;
+    // api.coeli.cat already returns plain values, no formatting needed
     return mapFunction({
       ...getSearchResponse,
-      ...{
-        entities: getSearchResponse.entities.map((e) =>
-          formattedEntity(language, e)
-        ),
-      },
       url:
         '/' + controlledSearchResponse.self.href.split('/').slice(3).join('/'),
     });
@@ -197,7 +195,7 @@ export class CoeliApi {
       `/${entity}/slugs/${slug}`,
       language,
       'GET',
-      (e) => mapFunction(formattedEntity(language, e))
+      (e) => mapFunction(e)
     );
   };
   getEntityById = async <T>(
@@ -210,7 +208,7 @@ export class CoeliApi {
       `/${entity}/${id}`,
       language,
       'GET',
-      (e) => mapFunction(formattedEntity(language, e))
+      (e) => mapFunction(e)
     );
   };
   getEntities = async <T>(
@@ -224,9 +222,7 @@ export class CoeliApi {
     >(`/${entity}/`, language, 'GET', (x: GetResponse<Entity>) => {
       return {
         ...x,
-        entities: x.entities
-          .map((e) => formattedEntity(language, e as Entity))
-          .map(mapFunction),
+        entities: x.entities.map((e) => mapFunction(e as Entity)),
       };
     });
     return coeliEntityGetResponse;
